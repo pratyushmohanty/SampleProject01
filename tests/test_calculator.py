4dd262345961c22cc7abd842f13b9a988c282770
Pratyush <mohanty.pratyush@gail.com>
2019-02-06 17:49:46 +0000

    broken test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,7 +8,7 @@ import unittest
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        assert 10 == calc.add(5, 5)
+        assert 10 == calc.add(4, 5)
 
     def test_subtract(self):
         assert 0 == calc.subtract(5, 5)
